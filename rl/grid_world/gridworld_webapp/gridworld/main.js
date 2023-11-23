303172fca02aa537a78aafcd15a16baf92e7a021
James <a@gmail.com>
2023-11-23 17:18:56 -0500

    learning pyspark

--- rl/grid_world/gridworld_webapp/gridworld/main.js
+++ rl/grid_world/gridworld_webapp/gridworld/main.js
@@ -1,1 +1,49 @@
 import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+  width: 500,
+  height: 500
+}
+
+const speedDown = 300
+
+class GameScene extends Phaser.Scene {
+  constructor(){ 
+    super('scene-game')
+  }
+
+  //preload assets
+  preload() {
+    this.load.image('background', '/')
+
+  }
+
+  create(){
+
+  }
+
+  update(){
+
+  }
+}
+
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: speedDown} ,
+      debug:true
+    }
+  },
+  scene: [GameScene]
+}
+
+
+
+const game = new Phaser.Game(config)
